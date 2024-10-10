9f7098076f9f9b000ffc2a35e51a17a3cd910375
Vitor Assis <vitorassis997@gmail.com>
2024-10-10 13:19:46 -0300

    repositórios

---
+++ source/repos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,50 @@
+﻿//Screen Sound 
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound!";
+
+
+void ExibirMensagemDeBoasVindas() //pascal case
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu ()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média da banda");
+    Console.WriteLine("Digite -1 para sair do menu");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine();
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhida); 
+            break;
+        case 2: Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhida); 
+            break;
+        case 3: Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhida);
+            break;
+        case 4: Console.WriteLine("Voce escolheu a opcao " + opcaoEscolhida);
+            break;
+        case -1: Console.WriteLine("Tchau :)");
+            break;
+        default: Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
+
